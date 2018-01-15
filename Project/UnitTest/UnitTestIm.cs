ea110c373d7910aa8b6ebb089356c49ab89d5424
Thibault MONTAUFRAY <amostrack@gmail.com>
2018-01-15 08:39:18 +0100

    update tests

--- Project/UnitTest/UnitTestIm.cs
+++ Project/UnitTest/UnitTestIm.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SlackClient;
 
 namespace SlackClient.Test
--- Project/UnitTest/UnitTestUser.cs
+++ Project/UnitTest/UnitTestUser.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SlackClient;
 using System.Collections.Generic;
 
